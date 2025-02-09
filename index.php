16ce64ac6a740a5272c940c44cca1ed04838aa53
adil mostapha EL AMRANI <adil.elamrani215@gmail.com>
2025-02-09 16:55:32 -0500

    Ajout des fichiers essentiels du thème

---
+++ index.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>4W4-Voyage</title>
+    <!-- <link rel="stylesheet" href="normalize.css"> -->
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap">
+    
+</head>
+
+<body>
+    <!-- Header Section -->
+    <header>
+        <div class="header-container">
+            <figure class="logo">
+                <img src="assets/logo/logo.png" alt="Logo">
+            </figure>
+            <nav class="navigation">
+                <ul class="menu">
+                    <li><a href="#">Aventure</a></li>
+                    <li><a href="#">Sportive</a></li>
+                    <li><a href="#">Culturelle</a></li>
+                    <li><a href="#">Zen</a></li>
+                </ul>
+            </nav>
+            <form class="search-bar">
+                <input type="text" placeholder="Rechercher...">
+                <button type="submit">
+                    <img src="https://s2.svgbox.net/hero-outline.svg?ic=search&color=000" alt="Search" width="15"
+                        height="15">
+                </button>
+            </form>
+        </div>
+    </header>
+
+    <!-- Hero Section -->
+    <section class="hero">
+        <div class="hero-content">
+            <h1>Bienvenue à notre club de voyage</h1>
+            <p>Découvrez un monde d'opportunités et d'aventures avec notre club de voyage. Que vous recherchiez des
+                escapades relaxantes, des expériences culturelles enrichissantes ou des défis sportifs, nous sommes là
+                pour transformer vos rêves en réalité. Rejoignez notre communauté et explorez les plus belles
+                destinations du monde avec confiance et sérénité.</p>
+            <a href="mailto:info@cmaisonneuve.qc.ca" class="contact-email">info@cmaisonneuve.qc.ca</a>
+            <button class="cta-button">Inscription</button>
+            <div class="social-icons">
+                <img src="https://s2.svgbox.net/social.svg?ic=facebook&color=000" alt="Facebook" width="20" height="20">
+                <img src="https://s2.svgbox.net/social.svg?ic=linkedin&color=000" alt="LinkedIn" width="20" height="20">
+                <img src="https://s2.svgbox.net/social.svg?ic=paypal&color=000" alt="Paypal" width="20" height="20">
+                <img src="https://s2.svgbox.net/social.svg?ic=instagram&color=000" alt="instagram" width="20"
+                    height="20">
+                <img src="https://s2.svgbox.net/social.svg?ic=youtube&color=000" alt="youtube" width="20" height="20">
+                <img src="https://s2.svgbox.net/social.svg?ic=snapchat&color=000" alt="snapchat" width="20" height="20">
+                <img src="https://s2.svgbox.net/social.svg?ic=tiktok&color=000" alt="tiktok" width="20" height="20">
+            </div>
+        </div>
+    </section>
+
+    <!-- Registration Form -->
+    <section class="registration-form">
+        <h2>Inscrivez-vous maintenant</h2>
+        <form>
+            <input type="text" placeholder="Nom" required>
+            <input type="text" placeholder="Prénom" required>
+            <input type="email" placeholder="Courriel" required>
+            <input type="tel" placeholder="Téléphone" required>
+            <button type="submit">S'INSCRIRE</button>
+        </form>
+    </section>
+
+    <!-- Promotions Section -->
+    <section class="photo-grid-container">
+        <h2 class="photo-grid-title">Nos destinations favorites</h2>
+        <div class="photo-grid">
+            <img src="assets/images/Barcelone, Espagne.jpg" alt="Barcelone, Espagne">
+            <img src="assets/images/Berlin, Allemagne.jpg" alt="Berlin, Allemagne">
+            <img src="assets/images/Budapest, Hongrie.jpg" alt="Budapest, Hongrie">
+            <img src="assets/images/Buenos Aires, Argentine.jpg" alt="Buenos Aires, Argentine">
+            <img src="assets/images/Chiang Mai, Thaïlande.jpg" alt="Chiang Mai, Thaïlande">
+            <img src="assets/images/Chicago, Illinois.jpg" alt="Chicago, Illinois">
+            <img src="assets/images/Dubaï, Emirats Arabes Unis.jpg" alt="Dubaï, Emirats Arabes Unis">
+            <img src="assets/images/Istanbul, Turquie.jpg" alt="Istanbul, Turquie">
+            <img src="assets/images/Florence, Italie.jpg" alt="Florence, Italie">
+            <img src="assets/images/Lisbonne, Portugal.jpg" alt="Lisbonne, Portugal">
+            <img src="assets/images/Marrakech, Maroc.jpg" alt="Marrakech, Maroc">
+            <img src="assets/images/Sydney, Australie.jpg" alt="Sydney, Australie">
+            <img src="assets/images/Siem Reap, Cambodge.jpg" alt="Siem Reap, Cambodge">
+            <img src="assets/images/Shanghai, Chine.jpg" alt="Shanghai, Chine">
+            <img src="assets/images/San Francisco Californie.jpg" alt="San Francisco Californie">
+        </div>
+    </section>
+
+    <!-- Footer Section -->
+    <footer>
+        <div class="footer-container">
+            <p>&copy; 2025 Club de Voyage. Tous droits réservés.</p>
+        </div>
+    </footer>
+</body>
+
+</html>
